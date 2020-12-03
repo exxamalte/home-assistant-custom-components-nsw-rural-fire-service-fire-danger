f89409b3761e32cfa4fc98749bee609ad2b556a5
Malte Franken <exxamalte@users.noreply.github.com>
2020-12-03 19:56:36 +1100

    Migrated to isort (#11)
    * isort configuration
    
    * isort compatible with black
    
    * isort formatted

--- custom_components/nsw_rural_fire_service_fire_danger/__init__.py
+++ custom_components/nsw_rural_fire_service_fire_danger/__init__.py
@@ -1,40 +1,39 @@
 # NSW Rural Fire Service Fire Danger.
 import asyncio
-from datetime import timedelta
 import logging
-from pyexpat import ExpatError
+from datetime import timedelta
 
 import voluptuous as vol
 import xmltodict
 from homeassistant.components.rest.data import RestData
-
 from homeassistant.config_entries import SOURCE_IMPORT, ConfigEntry
 from homeassistant.const import (
     CONF_SCAN_INTERVAL,
-    STATE_OK,
-    STATE_UNKNOWN,
     MAJOR_VERSION,
     MINOR_VERSION,
+    STATE_OK,
+    STATE_UNKNOWN,
 )
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers.dispatcher import async_dispatcher_send
 from homeassistant.helpers.event import async_track_time_interval
 from homeassistant.helpers.typing import ConfigType
+from pyexpat import ExpatError
 
 from .config_flow import configured_instances
 from .const import (
-    DEFAULT_SCAN_INTERVAL,
-    DOMAIN,
+    COMPONENTS,
     CONF_DISTRICT_NAME,
     DEFAULT_METHOD,
-    URL,
+    DEFAULT_SCAN_INTERVAL,
     DEFAULT_VERIFY_SSL,
-    XML_FIRE_DANGER_MAP,
-    XML_DISTRICT,
-    XML_NAME,
+    DOMAIN,
     SENSOR_ATTRIBUTES,
-    COMPONENTS,
+    URL,
+    XML_DISTRICT,
+    XML_FIRE_DANGER_MAP,
+    XML_NAME,
 )
 
 _LOGGER = logging.getLogger(__name__)
